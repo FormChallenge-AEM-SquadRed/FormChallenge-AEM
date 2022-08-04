685409191be29cc409599fd315831d62fa8c7b9c
WelbertJr <welbertjunior@live.com>
2022-08-03 21:02:07 -0300

    Chore:adding general project definitions

--- ui.frontend/src/App.js
+++ ui.frontend/src/App.js
@@ -1,14 +1,19 @@
-import { Page, withModel } from '@adobe/aem-react-editable-components';
-import React from 'react';
-
+import { Page, withModel } from "@adobe/aem-react-editable-components";
+import React from "react";
+import TabsProvider from '../src/components/contexts/TabsProvider';
+import UserDataProvider from '../src/components/contexts/UserDataProvider';
 // This component is the application entry point
 class App extends Page {
   render() {
     return (
-      <div>
-        {this.childComponents}
-        {this.childPages}
-      </div>
+      <TabsProvider>
+        <UserDataProvider>
+          <div>
+            {this.childComponents}
+            {this.childPages}
+          </div>
+        </UserDataProvider>
+      </TabsProvider>
     );
   }
 }
