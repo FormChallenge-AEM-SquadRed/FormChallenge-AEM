aaefecd8baf97172baa075f628e87dd16e924ad6
Jean-Stefan <jeanss12@gmail.com>
2022-08-10 15:45:57 -0300

    Style: removed responsive style from title

--- ui.frontend/src/components/Title/style.js
+++ ui.frontend/src/components/Title/style.js
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 
 export const ContainerTitle = styled.div`
-    @media (max-width: 600px) {
-        margin-bottom: 0.1rem;
-    }
     &.none {
         display: none;
     }
@@ -11,9 +8,4 @@ export const ContainerTitle = styled.div`
 export const TitleStyled = styled.h1`
     font-size: 1.8rem;
     color: ${({color}) => color || "#111111"};
-
-    @media (max-width: 600px) {
-        font-size: 1.8rem;
-        margin-bottom: 10px;
-    }
 `;
