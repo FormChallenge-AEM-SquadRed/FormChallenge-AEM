179e9ea3bc824a1a7244e6b4a8bda0af95066c79
Rafael Brandão <rafaelbrandaobastos@gmail.com>
2022-08-15 15:22:18 -0300

    Fix: Fixing Divs

--- ui.frontend/src/components/FormSocial/indexStyled.js
+++ ui.frontend/src/components/FormSocial/indexStyled.js
@@ -1,23 +1,17 @@
 import styled from 'styled-components';
 
 export const ContainerInputs = styled.div`
-    outline-style: solid;
     display: flex;  
     width: 100%;  
 `;
 
 export const InputContainer = styled.div`
-    outline-style: solid;
-    outline-color: green;
     width: 100%;    
 `;
 export const ContainerButtons = styled.div`
-    outline-style: solid;
     width: 100%;
 `;
 export const ButtonContainer = styled.div`
-    outline-style: solid;
-    outline-color: blue;
     width: 100%;
     padding-left: 81%;
     margin-top: 10px;
@@ -27,7 +21,6 @@ export const ButtonContainer = styled.div`
 
 
 export const ContainerForm = styled.div`
-    outline-style: solid; 
     display: flex;
     height: 100%;  
     flex-direction: column;
