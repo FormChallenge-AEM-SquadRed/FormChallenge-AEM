c686a5697c3789330c9cde6a2bd292da69686952
Jean-Stefan <jeanss12@gmail.com>
2022-08-16 16:32:30 -0300

    Style: change fonts based on props

--- ui.frontend/src/components/Input/style.js
+++ ui.frontend/src/components/Input/style.js
@@ -1,43 +1,43 @@
 import styled from "styled-components";
 
 export const InputGlobal = styled.input`
-  padding: 1.1rem 1.2rem;
-  width: 100%;
-  border: 2px solid #aaaaaa;
-  border-radius: 4px;
-  height: 4rem;
-  font-size: 1.6rem;
-  font-family: ${({ fonts }) => fonts || "Nunito"};
-  color: ${({ textcolor }) => textcolor || "#111111"};
-
-  ::placeholder {
+    padding: 1.1rem 1.2rem;
+    width: 100%;
+    border: 2px solid #aaaaaa;
+    border-radius: 4px;
+    height: 4rem;
     font-size: 1.6rem;
-    color: ${({ placeholdercolor }) => placeholdercolor || "#767676"};
-  }
+    font-family: ${({fonts}) => fonts || "Nunito"};
+    color: ${({textcolor}) => textcolor || "#111111"};
+
+    ::placeholder {
+        font-size: 1.6rem;
+        color: ${({placeholdercolor}) => placeholdercolor || "#767676"};
+    }
 `;
 
 export const ContainerEmail = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 65%;
+    display: flex;
+    flex-direction: column;
+    width: 65%;
 `;
 
 export const InputEmail = styled(InputGlobal)``;
 
 export const ContainerTel = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 25%;
+    display: flex;
+    flex-direction: column;
+    width: 25%;
 `;
 
 export const InputPhone = styled(InputGlobal)``;
 
 export const Label = styled.label`
-  font-size: 1.4rem;
-  color: ${({ labelcolor }) => labelcolor || "#767676"};
-  font-family: ${({ fonts }) => fonts || "Nunito"};
+    font-size: 1.4rem;
+    color: ${({labelcolor}) => labelcolor || "#767676"};
+    font-family: ${({fonts}) => fonts || "Nunito"};
 `;
 
 export const Container = styled.div`
-  width: 100%;
+    width: 100%;
 `;
