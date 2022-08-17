78188b3a07d8ff6b67c8b73143c4fd902175d4c9
Jean-Stefan <jeanss12@gmail.com>
2022-08-17 10:41:25 -0300

    Style: fixed components positioning

--- ui.frontend/src/components/FormSocial/index.js
+++ ui.frontend/src/components/FormSocial/index.js
@@ -1,80 +1,89 @@
-import React, { useContext } from "react";
-import { TabsContext } from "../../contexts/TabsProvider";
+import React, {useContext} from "react";
+import {TabsContext} from "../../contexts/TabsProvider";
 import Title from "../Title";
 import Tabs from "../Tabs";
 import Input from "../Input";
 import Button from "../Buttons";
-import { useForm } from "react-hook-form";
-import {InputContainer, ContainerInputs, ContainerForm, ButtonContainer, ContainerButtons} from "./indexStyled";
+import {useForm} from "react-hook-form";
+import {
+    InputContainer,
+    ContainerInputs,
+    ContainerForm,
+    ButtonContainer,
+    ContainerButtons,
+    ErrorMessage,
+} from "./style";
 
+const FormSocial = ({socialTitle, socialInput, socialButton}) => {
+    const [selectedTab, setSelectedTab] = useContext(TabsContext);
 
-const FormSocial = ({ socialTitle, socialInput, socialButton }) => {
-  const [selectedTab, setSelectedTab] = useContext(TabsContext);
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: {errors},
-    setValue,
-    getValues,
-  } = useForm(); 
-  console.log(socialInput[0].required) 
-  const onSubmit = () => {
-    setSelectedTab(selectedTab+1)
-  }                          
-  return (
-  
-    <>
-    
-      {selectedTab === 1 &&
-        socialTitle &&
-        socialTitle.map((item, index) => (
-          <Title 
-          key={index} 
-          color={item.titlecolor}>
-            {item.titletext}
-          </Title>
-        ))}
-      <Tabs />
-      <ContainerForm onSubmit = {handleSubmit(onSubmit)}>     
-      <ContainerInputs>  
-        {socialInput &&
-          socialInput.map((item, index) => (
-          
-            <InputContainer key = {index}>
-              <Input
-                key={index}
-                placeholder={item.placeholdertext}
-                type={item.types}
-                {...{ register: register(`${index}`, {required: item.required})}}
-              >
-                {item.labeltext}
-              </Input>
-              <p>
-                {errors[index] && 
-                  `${item.labeltext} is required`}
-              </p>
-            </InputContainer>            
-          ))}
-        </ContainerInputs>
-        <ContainerButtons>
-          {socialButton &&
-            socialButton.map((item, index) => (
-            <ButtonContainer>
-               <Button
-                key={index}
-                buttons={item.buttons}
-                text = {item.buttonlabel}
-                bgcolor = {item.backgroundcolor}
-                colortext = {item.buttonlabelcolor}
-              >          
-              </Button>
-            </ButtonContainer>  
-            ))}        
-        </ContainerButtons>   
-      </ContainerForm> 
-    
-    </>
-  );
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: {errors},
+        setValue,
+        getValues,
+    } = useForm();
+
+    const onSubmit = () => {
+        setSelectedTab(selectedTab + 1);
+    };
+
+    const regexp = {
+        text: /^[a-zA-Zà-úÀ-Ú]+(?:\s[a-zA-Zà-úÀ-Ú]+)+$/,
+        email: /^[a-z0-9._-]+(?:\.[a-z0-9._-]+)*@(?:[a-z0-9](?:[a-z-]*[a-z])?.)+[a-z](?:[a-z]*[a-z]){1,}?$/,
+        phone: /^[0-9]$/,
+    };
+    return (
+        <>
+            {selectedTab === 1 &&
+                socialTitle &&
+                socialTitle.map((item, index) => (
+                    <Title key={index} color={item.titlecolor}>
+                        {item.titletext}
+                    </Title>
+                ))}
+
+            <Tabs />
+
+            <ContainerForm onSubmit={handleSubmit(onSubmit)}>
+                <ContainerInputs>
+                    {socialInput &&
+                        socialInput.map((item, index) => (
+                            <Input
+                                key={index}
+                                placeholder={item.placeholdertext}
+                                type={item.types}
+                                {...{
+                                    register: register(`${item.labeltext}`, {
+                                        required: item.required,
+                                        pattern: regexp[item.types] ?? "",
+                                    }),
+                                }}
+                            >
+                                {item.labeltext}
+                                {errors[item.labeltext] && (
+                                    <ErrorMessage>is required</ErrorMessage>
+                                )}
+                            </Input>
+                        ))}
+                </ContainerInputs>
+
+                {socialButton &&
+                    socialButton.map((item, index) => (
+                        <ButtonContainer>
+                            <Button
+                                key={index}
+                                buttons={item.buttons}
+                                text={item.buttonlabel}
+                                bgcolor={item.backgroundcolor}
+                                colortext={item.buttonlabelcolor}
+                            ></Button>
+                        </ButtonContainer>
+                    ))}
+            </ContainerForm>
+        </>
+    );
 };
 export default FormSocial;
