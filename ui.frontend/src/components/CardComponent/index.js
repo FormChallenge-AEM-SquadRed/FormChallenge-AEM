b22d4c32c83106b54885b62b5fc6db667245d0d7
Jean-Stefan <jeanss12@gmail.com>
2022-08-17 18:33:52 -0300

    Feat: imported forms to card component

--- ui.frontend/src/components/CardComponent/index.js
+++ ui.frontend/src/components/CardComponent/index.js
@@ -2,43 +2,59 @@ import React from "react";
 import {useContext} from "react";
 import {MapTo} from "@adobe/aem-react-editable-components";
 import {Card, Container} from "./style";
-import Tabs from "../Tabs";
-import Title from "../Title";
 import {TabsContext} from "../../contexts/TabsProvider";
+import {UserDataContext} from "../../contexts/UserDataProvider";
+import FormBasic from "../FormBasic";
+import FormSocial from "../FormSocial";
+import FormCertificates from "../FormCertificates";
 
 const CardComponent = ({
     cardColor,
     basicTitle,
+    basicInput,
+    basicButton,
+    basicBirthday,
+    basicCheckbox,
     socialTitle,
+    socialInput,
+    socialButton,
+    certificatesBlock,
+    certificatesInput,
     certificatesTitle,
+    certificatesFinish,
 }) => {
     const [selectedTab, setSelectedTab] = useContext(TabsContext);
+    const [userData] = useContext(UserDataContext);
 
     return (
         <Container cardColor={cardColor}>
             <Card cardColor={cardColor}>
-                {selectedTab === 0 &&
-                    basicTitle &&
-                    basicTitle.map((item, index) => (
-                        <Title key={index} color={item.titlecolor}>
-                            {item.titletext}
-                        </Title>
-                    ))}
-                {selectedTab === 1 &&
-                    socialTitle &&
-                    socialTitle.map((item, index) => (
-                        <Title key={index} color={item.titlecolor}>
-                            {item.titletext}
-                        </Title>
-                    ))}
-                {selectedTab === 2 &&
-                    certificatesTitle &&
-                    certificatesTitle.map((item, index) => (
-                        <Title key={index} color={item.titlecolor}>
-                            {item.titletext}
-                        </Title>
-                    ))}
-                <Tabs />
+                {selectedTab === 0 && (
+                    <FormBasic
+                        basicButton={basicButton}
+                        basicTitle={basicTitle}
+                        basicInput={basicInput}
+                        basicBirthday={basicBirthday}
+                        basicCheckbox={basicCheckbox}
+                    />
+                )}
+                {selectedTab === 1 && (
+                    <FormSocial
+                        socialTitle={socialTitle}
+                        socialInput={socialInput}
+                        socialButton={socialButton}
+                    />
+                )}
+                {selectedTab === 2 && (
+                    <FormCertificates
+                        certificatesBlock={certificatesBlock}
+                        certificatesInput={certificatesInput}
+                        certificatesTitle={certificatesTitle}
+                        certificatesFinish={certificatesFinish}
+                    />
+                )}
+
+                {selectedTab === 3 && console.log(userData)}
             </Card>
         </Container>
     );
