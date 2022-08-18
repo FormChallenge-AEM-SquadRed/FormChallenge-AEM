9b90a3bf9b0ba33dfd94e550d8f458e56b897d19
Jean-Stefan <jeanss12@gmail.com>
2022-08-18 19:34:34 -0300

    Fix: font props import

--- ui.frontend/src/components/Birthday/select.js
+++ ui.frontend/src/components/Birthday/select.js
@@ -1,14 +1,25 @@
 import {StyledSelectContainer, StyledSelectLabel, StyledSelect} from "./style";
 
-const Select = ({ labelday, labelmonth, labelyear, children, register, colorinput, colortitle, fonts }) => {
+const Select = ({
+    labelday,
+    labelmonth,
+    labelyear,
+    children,
+    register,
+    colorinput,
+    colortitle,
+    fonts,
+}) => {
     return (
-      <StyledSelectContainer>
-        <StyledSelectLabel colortitle={colortitle}>
-          {labelday || labelmonth || labelyear}
-        </StyledSelectLabel>
-        <StyledSelect {...register} colorinput={colorinput} fonts={fonts}>{children}</StyledSelect>
-      </StyledSelectContainer>
+        <StyledSelectContainer>
+            <StyledSelectLabel colortitle={colortitle} fonts={fonts}>
+                {labelday || labelmonth || labelyear}
+            </StyledSelectLabel>
+            <StyledSelect {...register} colorinput={colorinput} fonts={fonts}>
+                {children}
+            </StyledSelect>
+        </StyledSelectContainer>
     );
-  };
-  
-  export default Select;
+};
+
+export default Select;
